43dda1a1e2f2e62c735cca55700251daeaf77e5f
Apolinario Katipunan <riokatipunan@gmail.com>
2023-11-01 13:36:13 +0800

    Added a helper function to convert excel to csv

---
+++ Deep Learning for Time Series Forecasting/Notebooks/excel2csv.py
@@ -0,0 +1,56 @@
+import os
+import pandas as pd
+import xlrd
+
+def read_excel(filesourcepath, resample = None):
+    
+    # read the excel file using pandas
+    df = pd.read_excel(filesourcepath)
+
+    # sometimes, excel does not return the value to the cell to which it was called; 
+    # sometimes it returns it to a cell below
+    if df.loc[4, 'Stock'] == 'Date':
+        df = pd.read_excel(filesourcepath, header = 5, engine='openpyxl')
+    elif df.loc[5, 'Stock'] == 'Date':
+        df = pd.read_excel(filesourcepath, header = 6, engine='openpyxl')
+
+    # delete all columns with NaN as values
+    # df = df.dropna(axis = 0, how = 'all')
+    df = df.dropna(axis = 0)
+
+    # we create a function that will transform the values of the numerical dates 
+    # from excel into python datetime
+    def read_date(date):
+        return xlrd.xldate.xldate_as_datetime(date, 0)
+
+    # apply the read_date function to the dataframe to convert the date column into datetime
+    df['Date'] = pd.to_datetime(df['Date'].apply(read_date), errors='coerce')
+    df.set_index(keys = 'Date', inplace = True)
+
+    # delete all columns with NaN as values
+    df = df.dropna(axis = 0)
+    
+    # resample the dataframe
+    if resample == 'W':
+        df = df.resample(rule = 'W').mean()
+    elif resample == 'M':
+        df = df.resample(rule = 'M').mean() 
+    
+    return df
+
+def excel2csv(sourcepath):
+    for file in os.listdir(sourcepath):
+        print(file)
+        filesourcepath = sourcepath + '/' + file
+        
+        # read the excel file and output a dataframe
+        df = read_excel(filesourcepath)
+
+        endfilesourcepath = "E:\Python\Deep Learning for Time Series Forecasting\Data\csv" + '/' + file
+        endfilesourcepath = endfilesourcepath.replace('.xlsx','.csv')
+        df.to_csv(endfilesourcepath)
+        
+            
+excel2csv("E:\Python\Deep Learning for Time Series Forecasting\Data\excel")
+
+    
