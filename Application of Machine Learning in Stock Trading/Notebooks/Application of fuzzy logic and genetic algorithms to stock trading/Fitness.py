776fae4edfffc84e604562a6b2afe1ac4fe42f35
Apolinario Katipunan <riokatipunan@gmail.com>
2024-01-23 20:12:03 +0800

    updated notebook

--- Application of Machine Learning in Stock Trading/Notebooks/Application of fuzzy logic and genetic algorithms to stock trading/Fitness.py
+++ Application of Machine Learning in Stock Trading/Notebooks/Application of fuzzy logic and genetic algorithms to stock trading/Fitness.py
@@ -65,12 +65,12 @@ def evaluate_fitness(series:pd.DataFrame, genome:Genome) -> list[Union[int, floa
     
     for i in range(0, len(stock.df)-1):
         # check condition for entry
-        condition1 = stock.df['z_sum_rolling'][i] >= genome.genome_dict["entry_condition"].value[1]
+        condition1 = stock.df['z_sum_rolling'].iat[i] >= genome.genome_dict["entry_condition"].value[1]
         condition2 = has_long_position is False
-        condition3 = stock.df['regime'][i] != -1
+        condition3 = stock.df['regime'].iat[i] != -1
         if condition1 and condition2 and condition3:
             # buy stock in the next day
-            stock.df['regime'][i+1] = 1
+            stock.df['regime'].iat[i+1] = 1
             has_long_position = True
             num_trades += 1
             buy_locator.append(stock.df.iloc[i])
@@ -79,27 +79,27 @@ def evaluate_fitness(series:pd.DataFrame, genome:Genome) -> list[Union[int, floa
             
         # establish trailing stop
         if has_long_position is True:
-            trailingstop *= stock.df['change'][i]
+            trailingstop *= stock.df['change'].iat[i]
                 
         # check conditions for exit
-        condition1 = stock.df['z_sum_rolling'][i] < genome.genome_dict["entry_condition"].value[0]
+        condition1 = stock.df['z_sum_rolling'].iat[i] < genome.genome_dict["entry_condition"].value[0]
         condition2 = trailingstop < genome.genome_dict["stop_loss"].value
         condition3 = has_long_position is True
         if( (condition1) or (condition2) ) and condition3:
-            stock.df['regime'][i+1] = -1
+            stock.df['regime'].iat[i+1] = -1
             has_long_position = False
             sell_locator.append(stock.df.iloc[i])
             trailingstop = 1
 
     # put 1 between 1 and -1 
     for i in range(1, len(stock.df)):
-        if stock.df['regime'][i-1] == 1 and stock.df['regime'][i] == 0:
-            stock.df['regime'][i] = 1
+        if stock.df['regime'].iat[i-1] == 1 and stock.df['regime'].iat[i] == 0:
+            stock.df['regime'].iat[i] = 1
 
     # compute returns
     for i in range(0, len(stock.df)):
-        if stock.df['regime'][i] != 0:
-            stock.df['returns'][i] = stock.df['change'][i]
+        if stock.df['regime'].iat[i] != 0:
+            stock.df['returns'].iat[i] = stock.df['change'].iat[i]
     
     # get returns for buy and hold strategy
     bnh_returns = stock.df['change'].cumprod()[-1]
@@ -116,11 +116,22 @@ def evaluate_fitness(series:pd.DataFrame, genome:Genome) -> list[Union[int, floa
 
     # compute for the sortino ration of the strategy
     strat_sortino_ratio = sortino_ratio(portfolio_returns = stock.df['returns'].cumprod()[-1], std_downside_portfolio_returns = downside_returns_std)
+
+    # compute for max drawdown
+    max_drawdown = np.ptp(stock.df["returns"])/stock.df["returns"].max()
     
     # check if sortino ratio is negative or NaN;
     # if it is negative or NaN, degenerate it into 0
-    if strat_sortino_ratio < 0 or np.isnan(strat_sortino_ratio):
+    if np.isnan(strat_sortino_ratio):
         strat_sortino_ratio = 0
+        
+    if strat_sortino_ratio < 0:
+        fitness = strat_sortino_ratio * num_trades * max_drawdown
+    else:
+        fitness = strat_sortino_ratio * (1/ (1+num_trades)) * (1-max_drawdown)
+        
+
+    
         
     
     # compute for the standard deviation of the strategy
@@ -129,8 +140,7 @@ def evaluate_fitness(series:pd.DataFrame, genome:Genome) -> list[Union[int, floa
     # compute for the sharpe ration of the strategy
     # strat_sharpe_ratio = sharpe_ratio(portfolio_returns = stock.df['returns'].cumprod()[-1], std_portfolio_returns = return_std)
     
-    # compute for max drawdown
-    # max_drawdown = np.ptp(stock.df["returns"])/stock.df["returns"].max()
+
 
     # compute for the total returns and plot these
     # stock.df['change'].cumprod().plot(label = 'Buy and hold')
@@ -140,7 +150,7 @@ def evaluate_fitness(series:pd.DataFrame, genome:Genome) -> list[Union[int, floa
     # show the percent change of the returns for buy and hold vs fuzzy logic
     # print(stock.df['change'].cumprod()[-1], stock.df['returns'].cumprod()[-1])
     
-    return strat_sortino_ratio
+    return fitness
     # return (stock, num_trades, bnh_returns, strat_returns, strat_sharpe_ratio, strat_sortino_ratio, max_drawdown)
 
 def sortino_ratio(portfolio_returns:float ,std_downside_portfolio_returns:float, risk_free_rate_returns:float = 2.5) -> float:
